30e601e9e4164af8431f10e130c92f0d16260782
Sebastian Fischer <sebf@informatik.uni-kiel.de>
2010-03-03 10:50:07 +0100

    fixed scaling

--- Graphics/BarChart.hs
+++ Graphics/BarChart.hs
@@ -8,7 +8,7 @@ module Graphics.BarChart (
 
   -- ^ Rendering bar charts
 
-  Config(..), defaultConfig, render, diagram, Measurable(..),
+  Config(..), conf, render, diagram, Measurable(..),
 
   ) where
 
@@ -34,40 +34,38 @@ instance Measurable Double where
   size = id
 
 data Config = Config {
-  filename :: FilePath,
-  barColors :: [SomeColor],
-  width, height :: Int,
-  ratio, padding, captionSize, labelSize, labelSep, barSep, barWidth :: Double
+  filename   :: FilePath,
+  bar_colors :: [SomeColor],
+  chart_size :: (Int,Int),
+  ratio, font_size, bar_ratio :: Double
  }
 
-defaultConfig :: Config
-defaultConfig =
-  Config { filename = "bar-chart.png",
-           barColors = cycle (map SomeColor
-            [midnightblue, firebrick, forestgreen]),
-           width = 600, height = 300, ratio = 1, padding = 10, captionSize = 12,
-           labelSize = 8, labelSep = 5, barSep = 100, barWidth = 20 }
+conf :: Config
+conf = Config { filename   = "bar-chart.png",
+                bar_colors = cycle (map SomeColor
+                                    [midnightblue, firebrick, forestgreen]),
+                chart_size = (600,300),
+                ratio = 1, font_size = 15, bar_ratio = 0.2 }
 
 render :: Measurable a => BarChart a -> IO ()
-render = renderWithConfig defaultConfig
+render = renderWith conf
 
-renderWithConfig :: Measurable a => Config -> BarChart a -> IO ()
-renderWithConfig config@Config{..} chart =
+renderWith :: Measurable a => Config -> BarChart a -> IO ()
+renderWith config@Config{..} chart =
   renderAs PNG filename (Width (fromIntegral width)) . diagram config $ chart
+ where (width,_) = chart_size
 
 diagram :: Measurable a => Config -> BarChart a -> Diagram
-diagram config@Config{..} chart = draw config{ ratio = hratio / wratio } chart
+diagram config@Config{..} chart@BarChart{..} =
+  draw config{ ratio = hratio / wratio, font_size = font_size / wratio } chart
  where
-  wratio = fromIntegral width / barChartWidth config chart
+  (width,height) = chart_size
+  wratio = fromIntegral width / genericLength bars
   hratio = fromIntegral height / barChartHeight chart
 
-barChartWidth :: Config -> BarChart a -> Double
-barChartWidth Config{..} BarChart{..} = genericLength bars * (barSep + barWidth)
-
 barChartHeight :: Measurable a => BarChart a -> Double
-barChartHeight BarChart{..}
-  | null bars = 0
-  | otherwise = maximum (map barSize bars)
+barChartHeight BarChart{..} | null bars = 0
+                            | otherwise = maximum (map barSize bars)
 
 barSize :: Measurable a => Bar a -> Double
 barSize Bar{..} | null blocks = 0
@@ -87,56 +85,62 @@ class Drawable a where
 
 instance Measurable a => Drawable (BarChart a) where
   draw config@Config{..} chart@BarChart{..} =
-    pad padding padding $
-      vsepA labelSep hcenter
-        [text captionSize caption,
-         hsep labelSep (map (draw config) (zip barColors blockLabels)),
-         vcat [hdistribA (barSep/2) left bottom
-                [yaxis, hdistribA barSep left bottom (map (draw config) bars)],
-              xaxis,
-              hdistribA (barSep/2) left bottom
-                [nil, hdistribA barSep left bottom
-                        (map (drawBarLabel config) bars)]]]
+    pad font_size font_size $ vsepA font_size hcenter [header,body]
    where
-    width  = barChartWidth config chart
-    height = ratio * barChartHeight chart
+    width   = genericLength bars
+    height  = ratio * barChartHeight chart
+    bar_sep = 1 - bar_ratio
 
-    xaxis = vsepA labelSep right [straight (pathFromVectors [(width,0)]),
-                                  text labelSize xlabel]
-    yaxis = vsep labelSep [text labelSize ylabel,
-                           straight (pathFromVectors [(0,height)])]
+    header  = vsepA font_size hcenter [title,legend]
+    title   = text (1.5*font_size) caption
+    legend  = hcat (map (draw config) (zip bar_colors blockLabels))
 
-drawBarLabel :: Config -> Bar a -> Diagram
-drawBarLabel Config{..} Bar{..} = text labelSize label
+    body    = vsepA font_size hcenter
+                [vcat [yBars, xaxis, xlabels], text font_size xlabel]
 
-instance Measurable a => Drawable (Bar a) where
-  draw config@Config{..} Bar{..} =
-    vcat (reverse (map (draw config) (zip barColors blocks)))
+    yBars   = hdistribA (bar_sep/2) left bottom [yaxis, cols]
+    yaxis   = vsep (font_size/2)
+                [text font_size ylabel, straight (pathFromVectors [(0,height)])]
+    cols    = sideBySide (map (draw config) bars)
+
+    xaxis   = straight (pathFromVectors [(width,0)]) <> hspace 0.5
+    xlabels = vcat [vspace (font_size/2),
+                    hcat [hspace (bar_sep/2),
+                          sideBySide (map (drawBarLabel config) bars)]]
 
 instance Drawable (SomeColor, Label) where
   draw Config{..} (color, string) =
-    hsep labelSep [block color labelSize labelSize, text labelSize string]
+    hcat [block color font_size font_size, text font_size string]
 
 block :: SomeColor -> Double -> Double -> Diagram
 block color width height =
   lineColor white . fillColor color $ roundRectF width height 0.1
 
-instance Measurable a => Drawable (SomeColor, Block a) where
-  draw Config{..} (color, Value x) =
-      hsep labelSep [block color barWidth (ratio * size x),
-                     ctext labelSize (show x)]
+instance Measurable a => Drawable (Bar a) where
+  draw config@Config{..} Bar{..} =
+    vcat (reverse (map (draw config) (zip bar_colors blocks)))
 
-  draw Config{..} (color,Interval{..}) =
-    hsep labelSep [block color barWidth (ratio * size mean),
-                   deviation,
-                   ctext labelSize (show mean)]
-   where deviation = translateY (ratio * size (mean-upper)) interval
-         interval  = vcat [bound,
-                           straight (pathFromVectors
-                                      [(0,ratio * size (upper-lower))]),
-                           bound]
-         bound     = translateX (-labelSep/2)
-                       (straight (pathFromVectors [(labelSep,0)]))
+instance Measurable a => Drawable (SomeColor, Block a) where
+  draw Config{..} (color, Value x) = hcat [bar, label]
+   where
+    bar   = block color bar_ratio (ratio * size x)
+    label = translateX (font_size/2) (ctext font_size (show x))
+
+  draw Config{..} (color,Interval{..}) = hcat [bar, deviation, label]
+   where
+    bar       = block color bar_ratio (ratio * size mean)
+    deviation = translateY (ratio * size (mean-upper)) interval
+    interval  = vcat [bound,line,bound]
+    bound     = translateX (-font_size/2) $
+                  straight (pathFromVectors [(font_size,0)])
+    line      = straight (pathFromVectors [(0,ratio * size (upper-lower))])
+    label     = translateX (-font_size/2) (ctext font_size (show mean))
 
 ctext :: Double -> String -> Diagram
 ctext size string = translateY (-size/2) (text size string)
+
+drawBarLabel :: Config -> Bar a -> Diagram
+drawBarLabel Config{..} Bar{..} = text font_size label
+
+sideBySide [] = nil
+sideBySide ds = hdistribA 1 left bottom (init ds) <> last ds
